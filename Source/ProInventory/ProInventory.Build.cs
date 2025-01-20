521871823f452af280b6d25b2ca328d807eb202c
Edvin <edvin.arn@hotmail.com>
2025-01-20 11:59:27 +0100

    Initial commit

---
+++ Source/ProInventory/ProInventory.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class ProInventory : ModuleRules
+{
+	public ProInventory(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "NavigationSystem", "AIModule", "Niagara", "EnhancedInput" });
+    }
+}
